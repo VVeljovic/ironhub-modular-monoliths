c712b13f854b4c28b6e54f0ef53b33200c4bbaf5
VVeljovic <veljkoveljovic13@gmail.com>
2026-05-13 10:39:44 +0200

    Entity model

---
+++ src/Shared/Shared.Domain/Entity.cs
@@ -0,0 +1,43 @@
+﻿namespace Shared.Domain
+{
+    public abstract class Entity : IEquatable<Entity>
+    {
+        protected Entity(Guid id)
+        {
+            Id = id;
+        }
+        
+        protected Entity() { }
+        
+        public Guid Id { get; private init; } 
+
+        public bool Equals(Entity? other)
+        {
+            if (other is null) return false; 
+
+            if(other.GetType() != GetType()) return false;
+
+            return Id == other.Id;
+        }
+
+        public static bool operator ==(Entity? left, Entity? right)
+        {
+            return left is not null && right is not null && left.Equals(right);
+        }
+
+        public static bool operator !=(Entity? left, Entity? right)
+        {
+            return !(left == right);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Entity entity && entity.GetType() == GetType() && entity.Id == Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode() * 41;
+        }
+    }
+}
