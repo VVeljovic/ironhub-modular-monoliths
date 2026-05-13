4b8b646138bcf5879feef19697049d1817b1cfda
VVeljovic <veljkoveljovic13@gmail.com>
2026-05-13 11:57:35 +0200

    domain events

---
+++ src/Shared/Shared.Domain/Primitives/DomainEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace Shared.Domain.Primitives
+{
+    public abstract record DomainEvent(Guid EventId, DateTime OccuredOn) : IDomainEvent
+    {
+        public DomainEvent() : this(Guid.NewGuid(), DateTime.UtcNow) { } 
+    }
+}
---
+++ src/Shared/Shared.Domain/Primitives/IDomainEvent.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Shared.Domain.Primitives
+{
+    public interface IDomainEvent : INotification
+    {
+        Guid EventId { get; }
+    
+        DateTime OccuredOn { get; }
+    }
+}
